1e10d83b790f93dc2f947e82057e823efedbaf93
Adarsh Chaudhary <144239960+77adarsh@users.noreply.github.com>
2025-01-28 10:37:44 +0530

    Implemented the message route for users, including the user ID

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 dotenv.config();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 const PORT = process.env.PORT || 3000;
 
